d50f5eaa97784eb98e39629645b7575fd766d20d
Luca Letizia <qdog912@gmail.com>
2022-04-09 02:13:33 -0700

    Added endlines to command list

--- parseCommands.js
+++ parseCommands.js
@@ -38,13 +38,13 @@ fs.readFile('index.js',function(err, indexData){
         // <SAM> Sam Commands
         README += '### Sam Commands - "Hey Sam, <command>"\n'
         for(var i = 1; i < samCommandList.length; i++){
-            README += "> " + samCommandList[i].toString();
+            README += "> " + samCommandList[i].toString() + "\n";
         }
 
         // <VC> Quote Commands
         README += "### Quote Commands\n"
         for(var i = 1; i < voiceCommandList.length; i++){
-            README += "> " + voiceCommandList[i].toString();
+            README += "> " + voiceCommandList[i].toString() + "\n";
         }
         fs.writeFile('README.md', README.toString(), {encoding: "utf8"},function(){});
     });
